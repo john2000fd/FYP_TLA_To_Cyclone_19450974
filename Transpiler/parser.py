8b05903c738a8a922f2ce43c08b1466a3c6cfcfa
u200262 <john.dunne.2020@mumail.ie>
2024-02-25 16:09:24 +0000

    work continues

--- Transpiler/parser.py
+++ Transpiler/parser.py
@@ -34,6 +34,16 @@ class VariablesNode(ASTNode):
     def __init__(self, variables):
         self.variables = variables  # List of variable names
 
+
+class InitNode(ASTNode):
+    def __init__(self, conditions):
+        self.conditions = conditions  # List of conditions in the initial state
+
+
+class NextNode(ASTNode):
+    def __init__(self, transitions):
+        self.transitions = transitions  # List of state transitions
+
 class AssignmentNode(ASTNode):
     def __init__(self, left, right):
         self.left = left
@@ -53,6 +63,51 @@ class LiteralNode(ASTNode):
     def __init__(self, value):
         self.value = value
 
+
+class GraphNode(ASTNode):
+    def __init__(self, identifier, body):
+        self.identifier = identifier  # The name of the graph
+        self.body = body  # List of graph statements (node and edge declarations)
+
+    def __repr__(self):
+        return f"Graph({self.identifier}, {self.body})"
+
+class NodeDeclaration(ASTNode):
+    def __init__(self, identifier):
+        self.identifier = identifier  # The name of the node
+
+    def __repr__(self):
+        return f"Node({self.identifier})"
+
+class EdgeDeclaration(ASTNode):
+    def __init__(self, from_node, to_node):
+        self.from_node = from_node  # Identifier of the starting node
+        self.to_node = to_node  # Identifier of the ending node
+
+    def __repr__(self):
+        return f"Edge({self.from_node} -> {self.to_node})"
+
+class InvariantNode(ASTNode):
+    def __init__(self, expression):
+        self.expression = expression  # The expression defining the invariant
+
+    def __repr__(self):
+        return f"Invariant({self.expression})"
+
+class PropertyGoalNode(ASTNode):
+    def __init__(self, expression):
+        self.expression = expression  # The expression defining the property goal
+
+    def __repr__(self):
+        return f"PropertyGoal({self.expression})"
+
+
+class SpecNode(ASTNode):
+    def __init__(self, init, next, invariant=None, property_goal=None):
+        self.init = init  # Initial state node
+        self.next = next  # Next state node
+        self.invariant = invariant  # Optional invariant node
+        self.property_goal = property_goal  # Optional property goal node
 
 
 # Parsing the module with optional extends and body (declarations, assignments, etc.)
@@ -98,6 +153,47 @@ def p_variables_declaration(p):
     'variables_declaration : VARIABLE IDENTIFIER_LIST'
     p[0] = VariablesNode(p[2])
  
+
+
+def p_graph_declaration(p):
+    'graph_declaration : GRAPH IDENTIFIER graph_body'
+    p[0] = GraphNode(p[2], p[3])
+
+
+def p_graph_body(p):
+    '''graph_body : graph_body graph_statement
+                  | graph_statement'''
+    if len(p) == 3:
+        p[1].append(p[2])
+        p[0] = p[1]
+    else:
+        p[0] = [p[1]]
+
+
+def p_graph_statement(p):
+    '''graph_statement : node_declaration
+                       | edge_declaration'''
+    p[0] = p[1]
+
+
+def p_node_declaration(p):
+    'node_declaration : NODE IDENTIFIER'
+    p[0] = NodeDeclaration(p[2])
+
+
+def p_edge_declaration(p):
+    'edge_declaration : EDGE IDENTIFIER ARROW IDENTIFIER'
+    p[0] = EdgeDeclaration(p[2], p[4])
+
+
+def p_invariant_declaration(p):
+    'invariant_declaration : INVARIANT expression'
+    p[0] = InvariantNode(p[2])
+
+
+def p_property_goal_declaration(p):
+    'property_goal_declaration : GOAL expression'
+    p[0] = PropertyGoalNode(p[2])
 
 
 def p_assignment_statement(p):
@@ -159,6 +255,13 @@ def p_identifier_list(p):
         p[0] = p[1]
     else:
         p[0] = [p[1]]
+
+
+def p_init_declaration(p):
+    'init_declaration : INIT EQUALS expression'
+    p[0] = InitNode(p[3])
+
+
 
 def p_empty(p):
     'empty :'
